449d356b0b0b3c064ce9b905aabd754981b8e1eb
David Brown <dmlb2000@gmail.com>
2018-06-18 22:56:28 -0700

    Declair the namespace right (#2)
    * Declair the namespace right
    
    * test directories shouldn't be proper python modules

--- pacifica/__init__.py
+++ pacifica/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """Pacifica Namespace."""
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+__import__('pkg_resources').declare_namespace(__name__)
