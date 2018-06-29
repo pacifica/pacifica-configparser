ed712905bd6ab34b05b833dc1d06f579ae9627b0
David Brown <dmlb2000@gmail.com>
2018-06-28 21:21:04 -0700

    refactored and split up into multiple methods

--- pacifica/configparser/__init__.py
+++ pacifica/configparser/__init__.py
@@ -13,16 +13,47 @@ except ImportError:  # pragma: no cover only one version of python will cover th
 import six
 
 
-# pylint: disable=too-few-public-methods
 class ConfigArgParser(object):
     """Pulls command line defaults from config file."""
+
+    @staticmethod
+    def get_safe_parser(conf_file):
+        """Return a safe config parser in versioned way."""
+        config = SafeConfigParser()
+        # seems python 2 uses readfp and python 3 uses read_file
+        # but pylint calls this as a deprecated method
+        # pylint: disable=deprecated-method
+        if six.PY2:  # pragma: no cover only with python 2
+            config.readfp(conf_file)
+        else:  # pragma: no cover only with python 3
+            config.read_file(conf_file)
+        # pylint: enable=deprecated-method
+        return config
+
+    @staticmethod
+    def get_action_group_titles(parser):
+        """Get the action group titles from a parser object."""
+        # pylint: disable=protected-access
+        return [x.title for x in parser._action_groups[2:] if hasattr(x, 'title')]
+        # pylint: enable=protected-access
+
+    @staticmethod
+    def update_defaults(defaults, conf_file, action_groups):
+        """Update the defaults from a config parser."""
+        if not conf_file:
+            return {}
+        def_copy = deepcopy(defaults)
+        config = ConfigArgParser.get_safe_parser(conf_file)
+        for config_group in action_groups:
+            def_copy.update(dict(config.items(config_group)))
+        def_copy.update(dict(config.items('Defaults')))
+        return def_copy
 
     @staticmethod
     def configargparser(parser, defaults, def_conf_file, env_prefix, argv=False):
         """Use defaults found in config file and return a Namespace."""
         if not argv:  # pragma: no cover within pytest
             argv = sys.argv
-        def_copy = deepcopy(defaults)
         config_parser = ArgumentParser(
             description='parse out the config file', add_help=False)
         config_parser.add_argument(
@@ -31,25 +62,12 @@ class ConfigArgParser(object):
             default=open(def_conf_file)
         )
         args, remaining_argv = config_parser.parse_known_args(argv)
-        if args.conf_file:
-            config = SafeConfigParser()
-            # seems python 2 uses readfp and python 3 uses read_file
-            # but pylint calls this as a deprecated method
-            # pylint: disable=deprecated-method
-            if six.PY2:  # pragma: no cover only with python 2
-                config.readfp(args.conf_file)
-            else:  # pragma: no cover only with python 3
-                config.read_file(args.conf_file)
-            # pylint: enable=deprecated-method
-            # pylint: disable=protected-access
-            for config_group in [x.title for x in parser._action_groups[2:] if hasattr(x, 'title')]:
-                def_copy.update(dict(config.items(config_group)))
-            # pylint: enable=protected-access
-            def_copy.update(dict(config.items('Defaults')))
+        action_group_titles = ConfigArgParser.get_action_group_titles(parser)
+        def_copy = ConfigArgParser.update_defaults(
+            defaults, args.conf_file, action_group_titles)
         for key in def_copy.keys():
             env_key = '{}_{}'.format(env_prefix.upper(), key.upper())
             if os.getenv(env_key, False):
                 def_copy[key] = os.getenv(env_key)
         parser.set_defaults(**def_copy)
         return parser.parse_args(remaining_argv)
-# pylint: enable=too-few-public-methods
